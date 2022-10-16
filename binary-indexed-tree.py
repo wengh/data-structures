e7c6f04f816e7ae8a865be6656d2215eaffe66fb
Weng Haoyu <whyharry9@gmail.com>
2022-10-15 23:44:58 -0400

    Update binary-indexed-tree.py

--- binary-indexed-tree.py
+++ binary-indexed-tree.py
@@ -25,15 +25,23 @@ class BinaryIndexedTree:
 class BinaryIndexedTree:
     def __init__(self, n):
         self.n = n
-        self.arr = [0] * (n+1)
-    def get(self, i):
+        self.arr = [0] * (n + 1)
+    def populate(self, values):
+        assert(len(values) == self.n)
+        for i in range(1, self.n + 1):
+            self.arr[i] += values[i - 1]
+            if i + (i & -i) <= self.n:
+                self.arr[i + (i & -i)] += self.arr[i]
+    def sum(self, i):
         ans = 0
         i += 1
         while i > 0:
             ans += self.arr[i]
             i -= i & -i
         return ans
-    def set(self, i, x):
+    def range(self, left, right):
+        return self.sum(right) - self.sum(left - 1)
+    def edit(self, i, x):
         i += 1
         while i <= self.n:
             self.arr[i] += x
