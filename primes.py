884b1c4198f8013a97594a4b1feb71e64676e716
Weng Haoyu <whyharry9@gmail.com>
2022-10-22 20:20:34 -0400

    Create primes.py

---
+++ primes.py
@@ -0,0 +1,40 @@
+from collections import Counter
+
+
+def getIsPrime(high):
+    mask = [True] * high
+    mask[0] = mask[1] = False
+    for n in range(2, high):
+        if mask[n]:
+            for i in range(n * n, high, n):
+                mask[i] = False
+    return mask
+
+high = 100000
+isPrime = getIsPrime(high)
+primes = [i for i, x in enumerate(isPrime) if x]
+
+
+def factorize(n):
+    i = 2
+    ans = Counter()
+    for p in primes:
+        if p * p > n:
+            break
+        while n % p == 0:
+            n //= p
+            ans[p] += 1
+    ans[n] += 1
+    return ans
+
+  
+def divisors(factors):
+    if len(factors) == 0:
+        yield 1
+    else:
+        p, n = factors[0]
+        a = 1
+        for i in range(n+1):
+            for b in divisors(factors[1:]):
+                yield a * b
+            a *= p
