38723fd127012446aa73460c99dc2a43f744dced
Weng Haoyu <whyharry9@gmail.com>
2023-02-25 22:33:57 -0500

    Update counter-nonzero.py

--- counter-nonzero.py
+++ counter-nonzero.py
@@ -1,6 +1,6 @@
 class Counter1(Counter):
-    def __setitem__(self, value, key):
+    def __setitem__(self, key, value):
         if value == 0:
             del self[key]
         else:
-            super().__setitem__(value, key)
+            super().__setitem__(key, value)
