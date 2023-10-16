2645a515150cb6ba9c567bdbb77053f1f0a7e3f9
Weng Haoyu <whyharry9@gmail.com>
2023-10-16 19:31:59 -0400

    Update monotonic-queue.cpp

--- monotonic-queue.cpp
+++ monotonic-queue.cpp
@@ -3,41 +3,29 @@
 
 template <typename T = int, typename Comp = std::less<T>>
 class MinMonotonicQueue {
-private:
-    struct Entry {
-        T x;
-        size_t count = 0;
-    };
-
 public:
     bool empty() const {
         return store.empty();
     }
 
     T min() const {
-        return store.front().x;
+        return store.front();
     }
 
     void push(T x) {
-        while (!store.empty() && lt(x, store.back().x)) {
+        while (!store.empty() && lt(x, store.back())) {
             store.pop_back();
         }
-        if (store.empty() || store.back().x != x) {
-            store.push_back({x, 1});
-        } else {
-            ++store.back().count;
-        }
+        store.push_back(x);
     }
 
     void pop(T x) {
-        if (!store.empty() && x == store.front().x) {
-            if (--store.front().count == 0) {
-                store.pop_front();
-            }
+        if (!store.empty() && x == store.front()) {
+            store.pop_front();
         }
     }
 
 private:
-    std::deque<Entry> store;
+    std::deque<T> store;
     static constexpr inline Comp lt{};
 };
